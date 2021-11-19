f514ca4f9b4fb1c792c2c06b51026b96154ccc74
mikohan <angara99@gmail.com>
2021-11-19 10:08:45 +0300

    Playing with travis yaml

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title="Recipe project swagger")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls'))
+    path("admin/", admin.site.urls),
+    path("api/user/", include("user.urls")),
+    path("api/", schema_view),
 ]
